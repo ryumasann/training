45f2ea0f73ecf5b41203f22617d1ef18024599e4
ryumasann <im20140319@gmail.com>
2020-07-01 16:45:39 +0900

    add comment

--- Python for biginner/eto.py
+++ Python for biginner/eto.py
@@ -1,5 +1,8 @@
+＃intで型変換可、:は’’の中に入れる。
 year = int(input('あなたの生年月日を4桁で記入してください。: '))
 eto_num = (year + 8) % 12
+#tupleは変更不可、Pythonの配列は異なる方のデータを入れられ、追加削除もできる動的配列。
 eto_tuple = ('子', '丑', '寅', '卯', '辰', '巳', '午', '未', '申', '酉', '戌', '亥')
 eto_name = eto_tuple[eto_num]
+#.formatでprintやstr型にある{}を変換する。
 print('あなたの干支は{}です。'.format(eto_name))
