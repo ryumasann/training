8358c6bef5d508add0da67e3e3ac97e539d3b299
ryumasann <im20140319@gmail.com>
2020-07-03 10:14:48 +0900

    input form

--- Python for biginner/pybot/pybotweb/pybotweb.py
+++ Python for biginner/pybot/pybotweb/pybotweb.py
@@ -1,10 +1,14 @@
-from bottle import route, run, template
+from bottle import route, run, template, request
 from datetime import datetime
 
 @route('/hello')
 def hello():
     now = datetime.now()
-    return template('pybot_template', now=now)
+    return template('pybot_template', text='')
 
+@route('/hello', method='POST')
+def do_hello():
+    input_text = request.forms.input_text
+    return template('pybot_template', text=input_text)
 
 run(host='localhost', port=8080, debug=True)
