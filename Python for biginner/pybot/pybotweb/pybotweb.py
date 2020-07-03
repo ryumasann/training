1b2e08ddad706f254bfb164853b54058339fdf0c
ryumasann <im20140319@gmail.com>
2020-07-03 09:40:55 +0900

    template

--- Python for biginner/pybot/pybotweb/pybotweb.py
+++ Python for biginner/pybot/pybotweb/pybotweb.py
@@ -1,7 +1,10 @@
-from bottle import route, run
+from bottle import route, run, template
+from datetime import datetime
 
 @route('/hello')
 def hello():
-    return 'Hello World!'
+    now = datetime.now()
+    return template('pybot_template', now=now)
+
 
 run(host='localhost', port=8080, debug=True)
