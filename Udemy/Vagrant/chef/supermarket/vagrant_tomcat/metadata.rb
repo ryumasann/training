5317949edf3a56c2967f1b20f9db615b975ffee0
ryumasann <im20140319@gmail.com>
2020-07-09 16:23:42 +0900

    write metadata.rb

---
+++ Udemy/Vagrant/chef/supermarket/vagrant_tomcat/metadata.rb
@@ -0,0 +1,12 @@
+name 'vagrant_tomcat'
+maintainer 'me'
+maintainer_email "foo@var.co.jp"
+license 'Appache 2.0'
+description 'something here'
+
+depends 'java'
+depends 'tomcat_latest'
+
+supports 'centos'
+
+recipe 
